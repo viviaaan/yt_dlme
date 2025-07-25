bb9ad4f2a7db19713752e2effcb7fe02d6bc339a
Vivian Samuel <viviansamuel42@gmail.com>
2025-07-25 22:20:30 +0530

    enable selecting custom resolution

--- src/Download.tsx
+++ src/Download.tsx
@@ -5,8 +5,8 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export default function Download({ url, id, title }) {
-    const [progress, setProgress] = useState({'percent': '0.0%'})
+export default function Download({ url, id, title, resolutions }) {
+    const [progress, setProgress] = useState({ 'percent': '0.0%' })
 
     async function progressView() {
         let finished = false;
@@ -28,21 +28,41 @@ export default function Download({ url, id, title }) {
 
     function downloadVideo(event) {
         const encodedURL = event.target.getAttribute('data-url')
+        const resmenu = document.getElementById('resolutionSelection')
+        const res = resmenu?.value
         setTimeout(progressView, 5000)
-        fetch('http://127.0.0.1:3000/download/' + encodedURL, { method: 'POST' }).then((response) => {
+        fetch('http://127.0.0.1:3000/download/' + encodedURL + '?res=' + res, { method: 'POST' }).then((response) => {
             response.blob().then((blob) => {
                 const url = URL.createObjectURL(blob)
                 const a = document.createElement('a');
                 a.href = url;
-                a.download =  `${title} (${id}).mkv`;
+                a.download = `${title} (${id}).mkv`;
                 a.click()
 
             })
         })
     }
 
+    let validResolutions = []
+    for (const resolution of resolutions) {
+        if (resolution !== 'audio only') {
+            validResolutions.push(resolution)
+        }
+    }
+    validResolutions.sort((a, b) => Number(b.split('x')[0]) - Number(a.split('x')[0]))
+    const resMenu = validResolutions.map((resolution) =>
+        <option key={resolution} value={resolution.split('x')[1]}>{resolution}</option>
+    )
+
+
     return (<div className='mt-10 gap-5 flex flex-col items-center'>
         <Progress progress={progress} />
-        <button className='text-xl text-black border-solid border-lime-300 bg-lime-300 border-1 rounded-2xl px-3 py-2 hover:cursor-pointer' onClick={downloadVideo} data-url={url}>Download</button>
+        <div className='flex flex-row items-center gap-3'>
+            <div>Quality:</div>
+            <select className='bg-[#1d1d1f] text-right rounded-2xl px-1.5 py-1' id='resolutionSelection'>
+                {resMenu}
+            </select>
+            <button className='text-xl text-black border-solid border-lime-300 bg-lime-300 border-1 rounded-2xl px-3 py-2 hover:cursor-pointer' onClick={downloadVideo} data-url={url}>Download</button>
+        </div>
     </div>)
 }
--- src/Progress.tsx
+++ src/Progress.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react"
-
 export default function Progress({ progress }) {
     if (progress.percent === '0.0%') {
         return <></>
     } else if (progress.percent === '100.0%') {
-        return <div>Finished Downloading!</div>
+        return <div className='text-[#f5f5f7]'>Finished Downloading!</div>
     } else {
         return (<>
             <div className='rounded-2xl w-[80vw] lg:w-[30vw] h-[30px] relative overflow-clip bg-neutral-900'>
                 <div className='bg-lime-300 h-full absolute' style={{ width: progress.percent }}></div>
-                <span className='w-full text-red-600 absolute text-center'>Downloading: {progress.percent}</span>
+                <span className='w-full text-[#f5f5f7] text-shadow-lg/100 text-shadow-black absolute text-center'>{progress.percent}</span>
             </div>
             <div>ETA: {progress.eta}</div>
         </>)
